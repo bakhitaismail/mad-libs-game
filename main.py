2c69c35ec3c5071e7a69d18f264e6acf4353c0d4
bakhitaismail <bakhitaismailk@gmail.com>
2022-05-10 07:55:53 +0300

    madlibs commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle import color
+
+
+language = input("Enter a language: ")
+name = input("Enter a name: ")
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+game = input("Enter a game: ")
+school = input("Enter a school: ")
+
+print("I speak {language}")
+print("My name is {name}")
+print("Roses are {color}")
+print("{plural noun} are blue")
+print("I love {celebrity}")
+print("Can we play {game}")
+print("I am a student at {school}")
